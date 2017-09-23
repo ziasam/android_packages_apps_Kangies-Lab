65558927b7241518ab03ebe1947bd52526911f61
beanstown106 <nbenis106@gmail.com>
2017-09-23 16:33:19 +0800

    lab: Fingerprint authentication vibration [2/2]
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/awaken/settings/Lockscreen.java
+++ src/com/awaken/settings/Lockscreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Project-Awaken
+ * Copyright (C) 2020 KangOS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
+import android.hardware.fingerprint.FingerprintManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -39,7 +40,7 @@ import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
-
+import com.android.internal.util.custom.FodUtils;
 import com.android.internal.logging.nano.MetricsProto;
 
 import java.util.ArrayList;
@@ -54,12 +55,15 @@ import com.dirtyunicorns.support.preferences.SystemSettingSwitchPreference;
 import com.dirtyunicorns.support.preferences.SystemSettingListPreference;
 
 
-@SearchIndexable
-public class Lockscreen extends SettingsPreferenceFragment {
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+public class Lockscreen extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
     private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker_category";
-
+    private FingerprintManager mFingerprintManager;
     private Preference mFODIconPicker;
+    private SwitchPreference mFingerprintVib;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -68,11 +72,40 @@ public class Lockscreen extends SettingsPreferenceFragment {
         PreferenceScreen prefSet = getPreferenceScreen();
         PackageManager packageManager = getContext().getPackageManager();
         boolean hasFod = packageManager.hasSystemFeature(CustomContextConstants.Features.FOD);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        final PackageManager mPm = getActivity().getPackageManager();
 
         mFODIconPicker = (Preference) findPreference(FOD_ICON_PICKER_CATEGORY);
         if (mFODIconPicker != null && !hasFod) {
             prefSet.removePreference(mFODIconPicker);
         }
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (mPm.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) &&
+                 mFingerprintManager != null) {
+            if (!mFingerprintManager.isHardwareDetected()){
+                prefScreen.removePreference(mFingerprintVib);
+            } else {
+                mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                        Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+                mFingerprintVib.setOnPreferenceChangeListener(this);
+            }
+        } else {
+            prefScreen.removePreference(mFingerprintVib);
+        }
+     }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFingerprintVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
